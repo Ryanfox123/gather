bb2890b6b2ca815cfc68c1b53d92d94da843500a
Ryan Fox <ryanfox1@hotmail.co.uk>
2025-05-05 23:50:22 +0100

    fix

--- src/lib/mongoClient.ts
+++ src/lib/mongoClient.ts
@@ -3,25 +3,16 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI!;
 const options = {};
 
-declare global {
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
+if (typeof global._mongoClientPromise === "undefined") {
+  global._mongoClientPromise = new MongoClient(uri, options).connect();
 }
 
-let client;
 let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env");
-}
-
 if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
+  const client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
